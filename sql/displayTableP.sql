a085980a904270a24142fec25e9af1f1ec5c63a9
jgrillo1 <jgrillo1@binghamton.edu>
2015-11-22 22:08:08 -0500

    Finished the displayTableP i think still cant test it

--- sql/displayTableP.sql
+++ sql/displayTableP.sql
@@ -3,6 +3,16 @@ type ref_cursor is ref cursor;
 
 function getEmployees
 	return ref_cursor;
+function getCustomers
+	return ref_cursor;
+function getProducts
+	return ref_cursor;
+function getSuppliers
+	return ref_cursor;
+function getSupply
+	return ref_cursor;
+function getPurchases
+	return ref_cursor;
 end displayTable; 
 /
 show errors
@@ -16,6 +26,47 @@ function getEmployees
 		select * from employees;
 		return rc;
 	end;
+
+function getCustomers
+	return ref_cursor as rc ref_cursor;
+	begin 
+		open rc for
+		select * from customers;
+		return rc;
+	end;
+
+function getProducts
+	return ref_cursor as rc ref_cursor;
+	begin
+		open rc for
+		select * from products;
+		return rc;
+	end;
+
+function getSuppliers
+	return ref_cursor as rc ref_cursor;
+	begin 
+		open rc for
+		select * from suppliers;
+		return rc;
+	end;
+
+function getSupply
+	return ref_cursor as rc ref_cursor;
+	begin
+		open rc for
+		select * from supply;
+		return rc;
+	end;
+
+function getPurchases
+	return ref_cursor as rc ref_cursor;
+	begin
+		open rc for
+		select * from purchases;
+		return rc;
+	end;
+
 end;
 /
 show errors
