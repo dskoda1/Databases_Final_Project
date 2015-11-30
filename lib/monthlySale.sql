94ade22db2daefa0a423e99291f7a88570e4d528
jgrillo1 <jgrillo1@binghamton.edu>
2015-11-29 22:35:31 -0500

    compiling now

--- lib/monthlySale.sql
+++ lib/monthlySale.sql
@@ -1,15 +1,20 @@
 create or replace package monthlySale as
-	procedure report_monthly_sale
-	(pid_in in products.pid%type);
+	type ref_cursor is ref cursor;
+	function report_monthly_sale
+	(pid_in in products.pid%type)
+	return ref_cursor;
 	
 
 end monthlySale;
 /
 
 create or replace package body monthlySale as
-	procedure report_monthly_sale
-	(pid_in in products.pid%type) is
+	function report_monthly_sale
+	(pid_in in products.pid%type) 
+	return ref_cursor as rc ref_cursor;
+	
 	begin
+		open rc for
 	 	select 	pname, 
 			to_char(ptime, 'MON') as Month, 
 			to_char(ptime, 'YYYY') as Year,
@@ -17,8 +22,9 @@ create or replace package body monthlySale as
 			sum(total_price) as TOTAL_SPENT,
 			(sum(total_price) / sum(qty)) as AVERAGE_PRICE
 		from products po, purchases pu
-		where po.pid = pid_in, po.pid = pu.pid
+		where po.pid = pu.pid and po.pid = pid_in
 		group by pname, to_char(ptime, 'MON'), to_char(ptime, 'YYYY');
+		return rc;
 	end;
 end monthlySale;
 /
